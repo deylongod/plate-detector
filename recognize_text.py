733d2b6c0abcfccf8ac9c5859ce2274a3c42e9cd
deylongod <deylon.shevtsov@yandex.ru>
2025-04-01 11:20:54 +0300

    (ver=0.0.2)
    recognize_text.py:
    - Добавлена предобработка изображений для точности распознавания
    - Реализован список допустимых символов для русских номеров
    
    crop_image.py:
    - Добавлено сохранение обработанных изображений для визуальной проверки и работы recognize_text.py

--- recognize_text.py
+++ recognize_text.py
@@ -1,15 +1,26 @@
+import cv2
 import easyocr
 
 
-def recognize_license_plate_easyocr(image_path):
-    reader = easyocr.Reader(['en'])
-    result = reader.readtext(image_path)
-    return result
+def recognize_license_plate(image_path):
+    image = cv2.imread(image_path)
+    
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    contrast = clahe.apply(gray)
+    blurred = cv2.GaussianBlur(contrast, (3, 3), 0)
+    _, thresh = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    cv2.imshow("ALPR", thresh)
+    cv2.waitKey(0)
+    
+    reader = easyocr.Reader(['ru'])
+    result = reader.readtext(thresh, allowlist="АВЕКМНОРСТУХ0123456789", detail=0)
+    recognized_text = ''.join(result)
+    
+    return recognized_text
 
 
 if __name__ == "__main__":
-    image_path = "./tests/cropped_plate_0.jpg"
-    recognized_text = recognize_license_plate_easyocr(image_path)
-    for detection in recognized_text:
-        bbox, text, _ = detection
-        print(f"Номер машины: {text}")
+    image_path = "./cropped_images/C515HC142_plate.jpg"
+    text = recognize_license_plate(image_path)
+    print("Номер машины:", f"[{text}]")
